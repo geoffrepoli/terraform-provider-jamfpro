aa9065c81b9ece53581f899a532f35fff0495f50
geoff <8885886+geoffrepoli@users.noreply.github.com>
2024-10-30 16:15:42 +0100

    cleaned up comments

--- internal/resources/accounts/crud.go
+++ internal/resources/accounts/crud.go
@@ -9,7 +9,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
-// create is responsible for creating a new Jamf Pro Script in the remote system.
+// create is responsible for creating a new Jamf Pro Account in the remote system.
 func create(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	return common.Create(
 		ctx,
@@ -21,7 +21,7 @@ func create(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.
 	)
 }
 
-// read is responsible for reading the current state of a Jamf Pro Account Group Resource from the remote system.
+// read is responsible for reading the current state of a Jamf Pro Account Resource from the remote system.
 func read(ctx context.Context, d *schema.ResourceData, meta interface{}, cleanup bool) diag.Diagnostics {
 	return common.Read(
 		ctx,
@@ -43,7 +43,7 @@ func readNoCleanup(ctx context.Context, d *schema.ResourceData, meta interface{}
 	return read(ctx, d, meta, false)
 }
 
-// update is responsible for updating an existing Jamf Pro Account Group on the remote system.
+// update is responsible for updating an existing Jamf Pro Account on the remote system.
 func update(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	return common.Update(
 		ctx,
@@ -55,7 +55,7 @@ func update(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.
 	)
 }
 
-// delete is responsible for deleting a Jamf Pro account .
+// delete is responsible for deleting a Jamf Pro Account.
 func delete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	return common.Delete(
 		ctx,
